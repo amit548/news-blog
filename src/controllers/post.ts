aebe6c7c737129aaeeb0c687999dde9917355c89
amit548 <androidone548@gmail.com>
2021-04-07 03:01:57 +0530

    delete images and update images added

--- src/controllers/post.ts
+++ src/controllers/post.ts
@@ -184,7 +184,7 @@ const updatePost = async (req: Request, res: Response, next: NextFunction) => {
     if (Object.keys(errors).length > 0)
       throw createError(404, { body: errors });
 
-    if (post?.creator !== user.id || user.role !== 'admin')
+    if (post?.creator !== user.id && user.role !== 'admin')
       errors.user = 'You are not owner/admin of this post';
     if (Object.keys(errors).length > 0)
       throw createError(406, { body: errors });
@@ -193,6 +193,100 @@ const updatePost = async (req: Request, res: Response, next: NextFunction) => {
     if (description) post!.description = description;
     if (category) post!.category = category;
     if (status) post!.private = status;
+    if (req.files) {
+      if ((req.files as any).thumbnailImage) {
+        if (
+          (req.files as any).thumbnailImage.mimetype === 'image/jpeg' ||
+          (req.files as any).thumbnailImage.mimetype === 'image/png'
+        ) {
+          await deleteFile(post!.thumbnailImage);
+          const fileName =
+            makeId(8) + '-' + (req.files as any).thumbnailImage.name;
+          post!.thumbnailImage = fileName;
+          (req.files as any).thumbnailImage.mv(
+            join(__dirname, '../../public/images/' + fileName)
+          );
+        } else {
+          errors.thumbnailImage = 'Only jpeg, jpg & png accepted';
+        }
+      }
+
+      if ((req.files as any).image1) {
+        if (
+          (req.files as any).image1.mimetype === 'image/jpeg' ||
+          (req.files as any).image1.mimetype === 'image/png'
+        ) {
+          if (post!.image1) {
+            await deleteFile(post!.image1);
+          }
+          const fileName = makeId(8) + '-' + (req.files as any).image1.name;
+          post!.image1 = fileName;
+          (req.files as any).image1.mv(
+            join(__dirname, '../../public/images/' + fileName)
+          );
+        } else {
+          errors.image1 = 'Only jpeg, jpg & png accepted';
+        }
+      }
+
+      if ((req.files as any).image2) {
+        if (
+          (req.files as any).image2.mimetype === 'image/jpeg' ||
+          (req.files as any).image2.mimetype === 'image/png'
+        ) {
+          if (post!.image2) {
+            await deleteFile(post!.image2);
+          }
+          const fileName = makeId(8) + '-' + (req.files as any).image2.name;
+          post!.image2 = fileName;
+          (req.files as any).image2.mv(
+            join(__dirname, '../../public/images/' + fileName)
+          );
+        } else {
+          errors.image2 = 'Only jpeg, jpg & png accepted';
+        }
+      }
+
+      if ((req.files as any).image3) {
+        if (
+          (req.files as any).image3.mimetype === 'image/jpeg' ||
+          (req.files as any).image3.mimetype === 'image/png'
+        ) {
+          if (post!.image3) {
+            await deleteFile(post!.image3);
+          }
+          const fileName = makeId(8) + '-' + (req.files as any).image3.name;
+          post!.image3 = fileName;
+          (req.files as any).image3.mv(
+            join(__dirname, '../../public/images/' + fileName)
+          );
+        } else {
+          errors.image3 = 'Only jpeg, jpg & png accepted';
+        }
+      }
+
+      if ((req.files as any).image4) {
+        if (
+          (req.files as any).image4.mimetype === 'image/jpeg' ||
+          (req.files as any).image4.mimetype === 'image/png'
+        ) {
+          if (post!.image4) {
+            await deleteFile(post!.image4);
+          }
+          const fileName = makeId(8) + '-' + (req.files as any).image4.name;
+          post!.image4 = fileName;
+          (req.files as any).image4.mv(
+            join(__dirname, '../../public/images/' + fileName)
+          );
+        } else {
+          errors.image4 = 'Only jpeg, jpg & png accepted';
+        }
+      }
+    }
+
+    if (Object.keys(errors).length > 0)
+      throw createError(403, { body: errors });
+
     const updatedPost = await post?.save();
 
     res.status(200).json(updatedPost);
@@ -229,6 +323,7 @@ const deletePost = async (req: Request, res: Response, next: NextFunction) => {
     const creatorUser = await UserModel.findById(post?.creator);
 
     await post?.remove();
+    if (post?.thumbnailImage) await deleteFile(post?.thumbnailImage);
     if (post?.image1) await deleteFile(post?.image1);
     if (post?.image2) await deleteFile(post?.image2);
     if (post?.image3) await deleteFile(post?.image3);
