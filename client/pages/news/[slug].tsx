e088bb9ef14e3d0621013b6bff49ae8f14b604ae
amit548 <androidone548@gmail.com>
2021-05-16 00:21:46 +0530

    improved meta tags

--- client/pages/news/[slug].tsx
+++ client/pages/news/[slug].tsx
@@ -28,6 +28,7 @@ const NewsSlug = () => {
 
   const [loading, setLoading] = useState(true);
   const [loadedData, setLoadedData] = useState([]);
+  const [categoryPostKey, setCategoryPostKey] = useState('');
   const [postsAscategory, setPostsAscategory] = useState<any>({});
   const [error, setError] = useState<any>({});
   const [videoList, setVideoList] = useState([]);
@@ -97,94 +98,99 @@ const NewsSlug = () => {
   }, [loadedData]);
 
   return (
-    <Grid container spacing={1}>
-      {loading && (
-        <Grid item xs={12}>
-          <Box display="flex" justifyContent="center">
-            <CircularProgress />
-          </Box>
-        </Grid>
-      )}
-
-      {Object.keys(error).map((err) => (
-        <Alert severity="error" key={err} className={classes.alert}>
-          {error[err]}
-        </Alert>
-      ))}
-
-      <Grid item xs={12} md={9}>
-        <Grid container spacing={1}>
-          {!loading && Object.keys(postsAscategory).length > 0 ? (
-            <>
-              {Object.keys(postsAscategory).map((categoryPostKey, i) => (
-                <>
-                  <Head>
-                    <title>{categoryPostKey}</title>
-                    <meta
-                      name="description"
-                      content={description.substring(0, 159).concat('...')}
-                    />
-                    <meta property="og:title" content={categoryPostKey} />
-                    <meta
-                      property="og:description"
-                      content={description.substring(0, 159).concat('...')}
-                    />
-                    <meta property="twitter:title" content={categoryPostKey} />
-                    <meta
-                      property="twitter:description"
-                      content={description.substring(0, 159).concat('...')}
-                    />
-                    <meta name="keywords" content={categoryPostKey} />
-                    <meta name="robots" content="index, follow" />
-                  </Head>
-                  <News
-                    key={i}
-                    chipName={categoryPostKey}
-                    posts={postsAscategory[categoryPostKey]}
-                  />
-                </>
-              ))}
-              {pageCount > 1 && (
-                <Grid
-                  item
-                  xs={12}
-                  style={{
-                    width: '100%',
-                    display: 'flex',
-                    alignItems: 'center',
-                    justifyContent: 'center',
-                  }}
-                >
-                  <Pagination
-                    count={pageCount}
-                    page={page}
-                    onChange={(_, value) => {
-                      setPage(value);
-                    }}
-                  />
-                </Grid>
-              )}
-            </>
-          ) : (
-            <Typography variant="h5">No Post Found</Typography>
-          )}
-        </Grid>
-      </Grid>
-      <Grid item xs={12} md={3}>
-        <Grid container spacing={1}>
+    <>
+      <Head>
+        <title>{categoryPostKey}</title>
+        <meta
+          name="description"
+          content={description.substring(0, 159).concat('...')}
+        />
+        <meta property="og:title" content={categoryPostKey} />
+        <meta
+          property="og:description"
+          content={description.substring(0, 159).concat('...')}
+        />
+        <meta property="twitter:title" content={categoryPostKey} />
+        <meta
+          property="twitter:description"
+          content={description.substring(0, 159).concat('...')}
+        />
+        <meta name="keywords" content={categoryPostKey} />
+        <meta name="robots" content="index, follow" />
+      </Head>
+      <Grid container spacing={1}>
+        {loading && (
           <Grid item xs={12}>
-            <Typography variant="h5">Useful videos</Typography>
+            <Box display="flex" justifyContent="center">
+              <CircularProgress />
+            </Box>
           </Grid>
-          {videoList.length > 0 ? (
-            videoList.map((video) => <SideBar video={video} key={video._id} />)
-          ) : (
+        )}
+
+        {Object.keys(error).map((err) => (
+          <Alert severity="error" key={err} className={classes.alert}>
+            {error[err]}
+          </Alert>
+        ))}
+
+        <Grid item xs={12} md={9}>
+          <Grid container spacing={1}>
+            {!loading && Object.keys(postsAscategory).length > 0 ? (
+              <>
+                {Object.keys(postsAscategory).map((categoryPostKey, i) => {
+                  setCategoryPostKey(categoryPostKey);
+                  return (
+                    <News
+                      key={i}
+                      chipName={categoryPostKey}
+                      posts={postsAscategory[categoryPostKey]}
+                    />
+                  );
+                })}
+                {pageCount > 1 && (
+                  <Grid
+                    item
+                    xs={12}
+                    style={{
+                      width: '100%',
+                      display: 'flex',
+                      alignItems: 'center',
+                      justifyContent: 'center',
+                    }}
+                  >
+                    <Pagination
+                      count={pageCount}
+                      page={page}
+                      onChange={(_, value) => {
+                        setPage(value);
+                      }}
+                    />
+                  </Grid>
+                )}
+              </>
+            ) : (
+              <Typography variant="h5">No Post Found</Typography>
+            )}
+          </Grid>
+        </Grid>
+        <Grid item xs={12} md={3}>
+          <Grid container spacing={1}>
             <Grid item xs={12}>
-              <Typography>No videos found</Typography>
+              <Typography variant="h5">Useful videos</Typography>
             </Grid>
-          )}
+            {videoList.length > 0 ? (
+              videoList.map((video) => (
+                <SideBar video={video} key={video._id} />
+              ))
+            ) : (
+              <Grid item xs={12}>
+                <Typography>No videos found</Typography>
+              </Grid>
+            )}
+          </Grid>
         </Grid>
       </Grid>
-    </Grid>
+    </>
   );
 };
 
