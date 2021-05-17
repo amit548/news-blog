87e8c941ad3630ed30a6fe2d2b27fd03a5fe1c9f
amit548 <androidone548@gmail.com>
2021-05-18 01:16:51 +0530

    public/images to client

--- src/server.ts
+++ src/server.ts
@@ -1,4 +1,3 @@
-import { join } from 'path';
 import { config } from 'dotenv';
 import express, { NextFunction, Request, Response } from 'express';
 import { connect } from 'mongoose';
@@ -27,8 +26,6 @@ server.use(cors({ credentials: true, origin: true }));
 server.use(cookieParser());
 server.use(expressFileupload());
 server.use(morgan('dev'));
-
-server.use('/public', express.static(join(__dirname, '../public')));
 
 server.use('/api/user', userRoutes);
 server.use('/api/me', meRoutes);
