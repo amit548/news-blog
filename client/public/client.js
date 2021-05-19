70316069720b8b55dd2bfaeb47c2b5b1ace6b9aa
amit548 <androidone548@gmail.com>
2021-05-20 02:42:16 +0530

    push notification added

--- client/public/client.js
+++ client/public/client.js
@@ -26,11 +26,10 @@ if ('serviceWorker' in navigator) {
           userVisibleOnly: true,
           applicationServerKey: urlBase64ToUint8Array(publicKey),
         });
-
-        const permission = await window.Notification.requestPermission();
-        if (permission !== 'granted') {
-          console.error('Permission not granted for Notification');
-        }
+      }
+      const permission = await window.Notification.requestPermission();
+      if (permission !== 'granted') {
+        console.error('Permission not granted for Notification');
       }
     } catch (_) {}
   });
