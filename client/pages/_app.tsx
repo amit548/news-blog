9b99704eb799b2f4e6a644d3fcf36dd5a8167ad9
amit548 <androidone548@gmail.com>
2021-05-19 15:41:06 +0530

    progress bar added

--- client/pages/_app.tsx
+++ client/pages/_app.tsx
@@ -1,7 +1,9 @@
+import dynamic from 'next/dynamic';
 import CssBaseline from '@material-ui/core/CssBaseline';
 import { ThemeProvider } from '@material-ui/core/styles';
 import { useEffect } from 'react';
 import axios from 'axios';
+import purple from '@material-ui/core/colors/purple';
 import 'react-responsive-carousel/lib/styles/carousel.min.css';
 
 import Layout from '../components/Layout';
@@ -10,6 +12,10 @@ import UserContextProvider from '../context/UserContext';
 import theme from '../src/theme';
 
 axios.defaults.baseURL = process.env.NEXT_PUBLIC_SERVER_BASE_URL;
+
+const NextNprogress = dynamic(() => import('nextjs-progressbar'), {
+  ssr: false,
+});
 
 const App = ({ Component, pageProps }) => {
   useEffect(() => {
@@ -25,6 +31,12 @@ const App = ({ Component, pageProps }) => {
       <AuthContextProvider>
         <UserContextProvider>
           <Layout>
+            <NextNprogress
+              color={purple[500]}
+              startPosition={0.3}
+              stopDelayMs={200}
+              height={2}
+            />
             <Component {...pageProps} />
           </Layout>
         </UserContextProvider>
