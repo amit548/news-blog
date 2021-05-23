e911ec218aad3e132716f5140ec1b1329dd869c4
amit548 <androidone548@gmail.com>
2021-05-24 01:02:16 +0530

    test notification

--- client/public/sw.js
+++ client/public/sw.js
@@ -4,24 +4,25 @@ self.addEventListener('push', (e) => {
     icon: '/android-chrome-192x192.png',
     body: 'Notified by Kormer Khoj',
     image: data.img,
+    click_action: 'http://google.com',
   });
 });
 
-self.addEventListener('notificationclick', function (event) {
-  const data = event.data.json();
-  let url = 'https://kormerkhoj.com/post/' + data._id;
-  event.notification.close();
-  event.waitUntil(
-    clients.matchAll({ type: 'window' }).then((windowClients) => {
-      for (var i = 0; i < windowClients.length; i++) {
-        var client = windowClients[i];
-        if (client.url === url && 'focus' in client) {
-          return client.focus();
-        }
-      }
-      if (clients.openWindow) {
-        return clients.openWindow(url);
-      }
-    })
-  );
-});
+// self.addEventListener('notificationclick', function (event) {
+//   const data = event.data.json();
+//   let url = 'https://kormerkhoj.com/post/' + data._id;
+//   event.notification.close();
+//   event.waitUntil(
+//     clients.matchAll({ type: 'window' }).then((windowClients) => {
+//       for (var i = 0; i < windowClients.length; i++) {
+//         var client = windowClients[i];
+//         if (client.url === url && 'focus' in client) {
+//           return client.focus();
+//         }
+//       }
+//       if (clients.openWindow) {
+//         return clients.openWindow(url);
+//       }
+//     })
+//   );
+// });
