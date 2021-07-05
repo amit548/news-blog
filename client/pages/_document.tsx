69d4e42976595b1407482c001ed2650cfc6b1d3b
amit548 <androidone548@gmail.com>
2021-07-05 19:00:25 +0530

    ad link added

--- client/pages/_document.tsx
+++ client/pages/_document.tsx
@@ -52,11 +52,11 @@ export default class MyDocument extends Document {
             `,
             }}
           />
-          {/* <script
+          <script
             data-ad-client="ca-pub-3501803361714853"
             async
             src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"
-          /> */}
+          />
         </Head>
         <body>
           <Main />
