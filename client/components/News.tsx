e6bf8be81c9a63e730d92f213cbf9f38505db488
amit548 <androidone548@gmail.com>
2021-07-13 00:39:56 +0530

    news chip to simple test

--- client/components/News.tsx
+++ client/components/News.tsx
@@ -1,6 +1,8 @@
 import dynamic from 'next/dynamic';
 import Grid from '@material-ui/core/Grid';
-import Chip from '@material-ui/core/Chip';
+import Divider from '@material-ui/core/Divider';
+import Typography from '@material-ui/core/Typography';
+import pink from '@material-ui/core/colors/pink';
 
 import PostCard from './PostCard';
 
@@ -10,16 +12,24 @@ const News = ({ chipName, posts }) => {
   return (
     <>
       <Grid item xs={12}>
-        <Chip
-          clickable
-          color="primary"
-          label={chipName}
+        <Typography
+          style={{
+            cursor: 'pointer',
+            display: 'inline-block',
+            marginBottom: '5px',
+            color: pink[500],
+            fontWeight: 'bold',
+          }}
+          variant="body1"
           onClick={() => {
             if (process.browser) {
               window.location.href = `/news/${chipName}/1`;
             }
           }}
-        />
+        >
+          {chipName}
+        </Typography>
+        <Divider style={{ backgroundColor: pink[500], padding: '1px' }} />
       </Grid>
 
       {posts &&
